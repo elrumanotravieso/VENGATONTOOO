0cfa3cf4336ad8140b5c140ae33a4bdd226af64c
FranGarcia <36627147+elrumanotravieso@users.noreply.github.com>
2019-04-10 12:17:40 +0200

    bsjvbdsjklgbsjkbgkjdrbjkt

---
+++ src/Pricipal.java
@@ -0,0 +1,14 @@
+
+public class Pricipal {
+
+	public static void main(String[] args) {
+		String[] pachacho = {"HS", "HAHHAHAH"};
+		
+		for (int i = 0; i < pachacho.length; i++) {
+			System.out.println(pachacho[i]);
+		}
+		
+	
+	
+	}
+}
